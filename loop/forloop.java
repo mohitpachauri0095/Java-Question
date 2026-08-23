ef3544febe493b7c6f591feac8ffd4f9eebcb447
Mohit Pachauri <mohitpachauri095@gmail.com>
2026-08-24 00:08:54 +0530

    commit

---
+++ loop/forloop.java
@@ -0,0 +1,11 @@
+package loop;
+import java.util.Scanner;
+public class forloop {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        for (int i=1; i<10; i++){
+            System.out.println(i);
+        }
+        
+    }
+}
