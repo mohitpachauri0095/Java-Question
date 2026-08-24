6ce6c13fa119eb0b5699995114b39ebedaf21a38
Mohit Pachauri <mohitpachauri095@gmail.com>
2026-08-24 23:27:37 +0530

    commit

---
+++ loopspractice/numbersofproduct.java
@@ -0,0 +1,9 @@
+public class numbersofproduct {
+    public static void main(String [] args){
+        int mul = 1;
+        for (int i=1; i<=10; i++){
+            mul = mul * i;
+        }
+        System.out.println(mul);
+    }
+}
--- loopspractice/sumUsingloop.java
+++ loopspractice/sumUsingloop.java
@@ -3,7 +3,6 @@ public class sumUsingloop {
         int sum = 0;
         for (int i=1; i<=10; i++){
             sum = sum+i;
-            
         }
         System.out.println("Sum = " + sum);
     }
