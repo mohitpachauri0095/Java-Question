b50e32e30098ed2d288dc98c420df7244a94da8f
Mohit Pachauri <mohitpachauri095@gmail.com>
2026-08-25 00:34:42 +0530

    commit

---
+++ Comtative programing/modulo/NegativeModuloHandling.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class NegativeModuloHandling {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        long a = sc.nextLong();
+        long m = sc.nextLong();
+        long ans = ((a%m)+m)%m;
+        System.out.println(ans);
+
+    }
+}
