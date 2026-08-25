d1746576a077a5ff9c23dab514cd59677b799376
Mohit Pachauri <mohitpachauri095@gmail.com>
2026-08-26 00:02:31 +0530

    commit

--- loopspractice/NestedLoop.java
+++ loopspractice/NestedLoop.java
@@ -4,11 +4,12 @@ public class NestedLoop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int table = sc.nextInt();
-
+        for (table =1; table<=5; table++){
         for (int i = 1; i <= 10; i++) {
+          
             System.out.println(table + " x " + i + " = " + (table * i));
         }
-
+    }
         sc.close();
     }
 }
