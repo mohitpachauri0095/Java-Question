1b851522e206b3450bf53c3d72bb55ce39fa9da0
Mohit Pachauri <mohitpachauri095@gmail.com>
2026-09-01 23:58:11 +0530

    commit

---
+++ whileLoops/sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main (String args []){
+        int i = 1; 
+        int sum = 0;
+        while (i <=10){
+            System.out.println(i+sum);
+        }
+    }
+}
