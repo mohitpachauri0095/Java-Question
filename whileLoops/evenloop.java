3853f0876c19f8141ead0d6338611b6cdaf8a5cc
Mohit Pachauri <mohitpachauri095@gmail.com>
2026-09-01 23:44:13 +0530

    commit

--- whileLoops/evenloop.java
+++ whileLoops/evenloop.java
@@ -1,10 +1,12 @@
 public class evenloop {
     public static void main (String args []){
-        int i =20;
-        while (i % 2 == 1){
+        int i =1;
+        while (i <= 20){
+            if (i % 2 == 0){
             System.out.println(i);
-            i ++;
-            
+
+            }
+                        i ++;
             }
         }
     }
