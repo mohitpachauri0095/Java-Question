982ccf17bb5ce421be72f56ef7b7e0918cd15ddd
Mohit Pachauri <mohitpachauri095@gmail.com>
2026-09-03 23:09:23 +0530

    commit

---
+++ whileLoops/reverseNumber.java
@@ -0,0 +1,11 @@
+public class reverseNumber {
+    public static void main (String args []){
+        int i=0;
+        while (i < 5){
+              i++;
+            System.out.print(i);
+
+        }
+      
+    }
+}
