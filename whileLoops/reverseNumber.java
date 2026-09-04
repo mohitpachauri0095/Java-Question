59c283700359ac33824caa54bd8c5d0d88aa537e
Mohit Pachauri <mohitpachauri095@gmail.com>
2026-09-04 15:56:15 +0530

    commit

--- whileLoops/reverseNumber.java
+++ whileLoops/reverseNumber.java
@@ -1,13 +1,13 @@
 public class reverseNumber {
     public static void main (String args []){
-        int number = 123456789;
+        int number = 1234567890;
         int reverse = 0;
-        while (number > 0){
-            int digit = number % 10;
+        while (number >0){
+            int digit = number %10;
             reverse = reverse * 10 +digit;
-            number = number /10;
-
+            number = number / 10;
         }
-        System.out.print(reverse);
+
+        System.out.println(reverse);
     }
 }
