84a1e296cd26a382d9fbac1ee190cf7e2d55eec8
Mohit Pachauri <mohitpachauri095@gmail.com>
2026-09-09 22:22:29 +0530

    commit

--- java series types/whileLoops/countdigit.java
+++ java series types/whileLoops/countdigit.java
@@ -1,16 +1,17 @@
 
-// import java.util.*;
-public class countdigit {
-    public static void main(String args []){
-        // Scanner sc = new Scanner (System.in);
-        int number =12345;
-        int count = 1;
-        while (number >0){
-            number = number /10;
-            count++;
-        }
-        System.out.println(count);
+    // import java.util.*;
+    public class countdigit {
+        public static void main(String args []){
+            // Scanner sc = new Scanner (System.in);
+            int number =12345;
+            int count = 0;
+            while (number >0){
 
+            
+                count++;
+            }
+            System.out.println(count);
+
+        }
+        
     }
-    
-}
