1e9aaa34bb4382e7e194b475bfbab53de736052c
Mohit Pachauri <mohitpachauri095@gmail.com>
2026-09-10 17:23:44 +0530

    commit

---
+++ java series types/demo/lastdigittwogivinnum.java
@@ -0,0 +1,15 @@
+// package java series types.demo;
+
+public class lastdigittwogivinnum {
+    public static void main (String args []){
+        int num1 = 12345;
+        int num2 = 65433;
+
+        int digit1= num1%10;
+        int digit2= num2 %10;
+
+        int sum = digit1 + digit2;
+        System.out.println(sum);
+
+    }
+}
